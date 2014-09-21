b15b7b062936019f69e5b98c07681edd4043e1a1
Tiago Sarri <titisarri@gmail.com>
2014-09-21 19:07:40 -0300

    * Commit inicial

---
+++ ProjetoInicial/src/br/com/yaw/projetoinicial/Main.java
@@ -0,0 +1,23 @@
+package br.com.yaw.projetoinicial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String periodo = "manha";
+		
+		switch(periodo)
+		{
+			case "manha":
+				System.out.println("Bom dia");
+				break;
+			case "tarde":
+				System.out.println("Boa tarde");
+				break;
+			case "noite":
+				System.out.println("Boa noite");
+				break;				
+		}
+	}
+
+}
